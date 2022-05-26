259f4ffcf59076902bfdb49bff0d93f9aa442657
xg4 <xingor4@gmail.com>
2022-05-26 14:59:51 +0800

    chore: merge rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,54 +1,36 @@
-import babel from '@rollup/plugin-babel'
+import { babel } from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
-import resolve from '@rollup/plugin-node-resolve'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx']
 
-export default [
-  {
-    input: 'src/index.ts',
-    external: [
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
-    ],
-    output: [
-      {
-        file: pkg.exports,
-        format: 'esm',
-      },
-    ],
-    plugins: [
-      json(),
-      resolve({ extensions }),
-      commonjs(),
-      babel({
-        extensions,
-        include: ['src/**/*'],
-      }),
-      terser(),
-    ],
-  },
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        name: 'Boilerplate',
-        file: pkg.browser,
-        format: 'umd',
-      },
-    ],
-    plugins: [
-      json(),
-      resolve({ extensions }),
-      commonjs(),
-      babel({
-        extensions,
-        include: ['src/**/*'],
-      }),
-      terser(),
-    ],
-  },
-]
+export default {
+  input: 'src/index.ts',
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  output: [
+    {
+      file: pkg.exports,
+      format: 'esm',
+    },
+    {
+      name: 'Boilerplate',
+      file: pkg.browser,
+      format: 'umd',
+    },
+  ],
+  plugins: [
+    json(),
+    nodeResolve({ extensions }),
+    commonjs(),
+    babel({
+      extensions,
+    }),
+    terser(),
+  ],
+}
